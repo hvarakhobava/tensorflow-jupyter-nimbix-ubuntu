ab93d8d791d9cac5daa5ab0c398cd1a6347ef625
Hanna <h.varakhobava@gmail.com>
2017-06-10 21:29:32 +0300

    startup fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # open ports
-iptables -A  INPUT -p tcp --dport 8888 -j ACCEPT
-iptables -A  INPUT -p tcp --dport 6006 -j ACCEPT
-iptables -A  INPUT -p tcp --dport 5901 -j ACCEPT
-iptables -A  INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A  INPUT -p tcp --dport 8888 -j ACCEPT
+sudo iptables -A  INPUT -p tcp --dport 6006 -j ACCEPT
+sudo iptables -A  INPUT -p tcp --dport 5901 -j ACCEPT
+sudo iptables -A  INPUT -p tcp --dport 443 -j ACCEPT
 
-screen -d -m /run_jupyter.sh --allow-root
+sudo cp -r /root/.jupyter /home/nimbix/
+sudo cp /run_jupyter.sh /home/nimbix/
+screen -d -m /home/nimbix/run_jupyter.sh --allow-root
 screen -d -m tensorboard --logdir=/tmp/tensorboard
