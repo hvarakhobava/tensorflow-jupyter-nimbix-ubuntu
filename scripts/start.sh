cf6d4a28c85436ba9d60381f3cef98a7a79813da
Hanna <h.varakhobava@gmail.com>
2017-06-10 22:14:35 +0300

    jupyter run command fixed

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,5 @@ sudo iptables -A  INPUT -p tcp --dport 5901 -j ACCEPT
 sudo iptables -A  INPUT -p tcp --dport 443 -j ACCEPT
 
 sudo cp -r /root/.jupyter /home/nimbix/
-sudo cp /run_jupyter.sh /home/nimbix/
-screen -d -m /home/nimbix/run_jupyter.sh --allow-root
+screen -d -m jupyter-notebook
 screen -d -m tensorboard --logdir=/tmp/tensorboard
