9fb6af1f578e700b89a0280e715aa4a409841819
Hanna <h.varakhobava@gmail.com>
2017-06-13 16:23:12 +0300

    removed sudo from copying .jupyter files

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,6 @@ sudo iptables -A  INPUT -p tcp --dport 6006 -j ACCEPT
 sudo iptables -A  INPUT -p tcp --dport 5901 -j ACCEPT
 sudo iptables -A  INPUT -p tcp --dport 443 -j ACCEPT
 
-sudo cp -r /root/.jupyter /home/nimbix/
+cp -r /root/.jupyter /home/nimbix/
 jupyter-notebook & tensorboard --logdir=/tmp/tensorboard
 
