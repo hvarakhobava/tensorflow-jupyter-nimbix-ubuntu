cfe9afddd1767bf6e04749082c21351e9133f6b3
Hanna <h.varakhobava@gmail.com>
2017-06-13 18:14:39 +0300

    chown .jupyter dir to nimbix

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,7 @@ sudo iptables -A  INPUT -p tcp --dport 6006 -j ACCEPT
 sudo iptables -A  INPUT -p tcp --dport 5901 -j ACCEPT
 sudo iptables -A  INPUT -p tcp --dport 443 -j ACCEPT
 
-cp -r /root/.jupyter /home/nimbix/
+sudo cp -r /root/.jupyter /home/nimbix/
+sudo chown nimbix /home/nimbix/.jupyter
 jupyter-notebook & tensorboard --logdir=/tmp/tensorboard
 
